cd1aec22384b274e32dd3b5cbbd890f786fe60d0
Miano <dev.miano@gmail.com>
2022-03-19 00:54:52 +0300

    gave each pizza unique ids and arranged the js functions accordingly

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,79 @@
-var modal = document.getElementById('myModal');
-var close = document.getElementsByClassName('close')[0];
+// Pepperoni Functions
 
-var pepperoni = document.getElementById('pepperoniBtn');
-var hawaiian = document.getElementById('hawaiianBtn');
-var chicken = document.getElementById('chickenBtn');
-var veggie = document.getElementById('veggieBtn');
+var pepperoniModal = document.getElementById('pepperoniModal');
+var pepperoniClose = document.getElementById('pepperoniClose');
+var pepperoniBtn = document.getElementById('pepperoniBtn');
 
-pepperoni.onclick = function () {
-  modal.style.display = 'block';
-};
-hawaiian.onclick = function () {
-  modal.style.display = 'block';
-};
-chicken.onclick = function () {
-  modal.style.display = 'block';
-};
-veggie.onclick = function () {
-  modal.style.display = 'block';
+pepperoniBtn.onclick = function () {
+  pepperoniModal.style.display = 'block';
 };
 
-close.onclick = function () {
-  modal.style.display = 'none';
+pepperoniClose.onclick = function () {
+  pepperoniModal.style.display = 'none';
 };
 
 window.onclick = function (e) {
-  if (e.target == modal) {
-    modal.style.display = 'none';
+  if (e.target == pepperoniModal) {
+    pepperoniModal.style.display = 'none';
+  }
+};
+
+// Hawaiian Functions
+
+var hawaiianModal = document.getElementById('hawaiianModal');
+var hawaiianClose = document.getElementById('hawaiianClose');
+var hawaiianBtn = document.getElementById('hawaiianBtn');
+
+hawaiianBtn.onclick = function () {
+  hawaiianModal.style.display = 'block';
+};
+
+hawaiianClose.onclick = function () {
+  hawaiianModal.style.display = 'none';
+};
+
+window.onclick = function (e) {
+  if (e.target == hawaiianModal) {
+    hawaiianModal.style.display = 'none';
+  }
+};
+
+// Chicken Functions
+
+var chickenModal = document.getElementById('chickenModal');
+var chickenClose = document.getElementById('chickenClose');
+var chickenBtn = document.getElementById('chickenBtn');
+
+chickenBtn.onclick = function () {
+  chickenModal.style.display = 'block';
+};
+
+chickenClose.onclick = function () {
+  chickenModal.style.display = 'none';
+};
+
+window.onclick = function (e) {
+  if (e.target == chickenModal) {
+    chickenModal.style.display = 'none';
+  }
+};
+
+// Veggie Functions
+
+var veggieModal = document.getElementById('veggieModal');
+var veggieClose = document.getElementById('veggieClose');
+var veggieBtn = document.getElementById('veggieBtn');
+
+veggieBtn.onclick = function () {
+  veggieModal.style.display = 'block';
+};
+
+veggieClose.onclick = function () {
+  veggieModal.style.display = 'none';
+};
+
+window.onclick = function (e) {
+  if (e.target == veggieModal) {
+    veggieModal.style.display = 'none';
   }
 };
