6ca2c577f7d15fa82f82217073df208dc8a89a2b
Miano <dev.miano@gmail.com>
2022-03-21 10:08:05 +0300

    added function to display values

--- scripts/pepperoni.js
+++ scripts/pepperoni.js
@@ -15,10 +15,8 @@ export function pepperoni() {
   pepperoniOrder.addEventListener('click', function (e) {
     e.preventDefault();
     pepperoniModal.style.display = 'none';
-
     pepperoniOption = parseInt(pOption.value);
     pepperoniCrust = parseInt(pCrust.value);
-
     if (beef.checked == true && cheese.checked == true) {
       var beef1 = document.getElementById('beef').value;
       var cheese1 = document.getElementById('cheese').value;
@@ -30,13 +28,37 @@ export function pepperoni() {
       var beef2 = document.getElementById('beef').value;
       pepperoniToppings = parseInt(beef2);
     } else {
-      return alert('please,select atleast one');
+      return alert('please select atleast one topping');
     }
     pepperoniTotal = pepperoniOption + pepperoniCrust + pepperoniToppings;
-    alert(pepperoniTotal);
 
-    // Pepperoni JQuery
+    function addPepperoniOrder(
+      pepperoniOption,
+      pepperoniCrust,
+      pepperoniToppings,
+      pepperoniTotal
+    ) {
+      this.pepperoniOption = pepperoniOption;
+      this.pepperoniCrust = pepperoniCrust;
+      this.pepperoniToppings = pepperoniToppings;
+      this.pepperoniTotal = pepperoniTotal;
+    }
 
-    $(function () {});
+    var addToPepperoniOrder = new addPepperoniOrder(
+      pepperoniOption,
+      pepperoniCrust,
+      pepperoniToppings,
+      pepperoniTotal
+    );
+
+    var orderName = addToPepperoniOrder.pepperoniOption;
+    var orderCrust = addToPepperoniOrder.pepperoniCrust;
+    var orderToppings = addToPepperoniOrder.pepperoniToppings;
+    var orderTotal = addToPepperoniOrder.pepperoniTotal;
+
+    const newOrder = `<div><p>Pizza</p><p>${orderName}</p></div><div><p>Crust</p><p>${orderCrust}</p></div><div><p>Toppings</p><p>${orderToppings}</p></div><div><p>Total</p><p>${orderTotal}</p></div>`;
+
+    const list = document.querySelector('#orderCard');
+    list.insertAdjacentHTML('afterbegin', newOrder);
   });
 }
