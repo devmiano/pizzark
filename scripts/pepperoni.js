0882d052d0981772ef11e8b870196d3573cdff64
Miano <dev.miano@gmail.com>
2022-03-23 03:28:41 +0300

    corrected unresponsive functions

--- scripts/pepperoni.js
+++ scripts/pepperoni.js
@@ -9,10 +9,16 @@ var locationOrder = document.getElementById('locationOrder');
 var locationModal = document.getElementById('locationModal');
 var location = document.getElementById('location');
 var checkout = document.getElementById('checkout');
+var reset = document.getElementById('form_data').reset();
 var pepperoniToppings = 0;
 var subtotal = 0;
 var grandTotal = 0;
-var pepperoniOption, pepperoniCrust, pepperoniTotal;
+var pepperoniOption,
+  pepperoniCrust,
+  pepperoniTotal,
+  optionText,
+  crustText,
+  toppingsText;
 
 // Pepperoni Function
 export function pepperoni() {
@@ -25,12 +31,15 @@ export function pepperoni() {
       var beef1 = document.getElementById('beef').value;
       var cheese1 = document.getElementById('cheese').value;
       pepperoniToppings = parseInt(beef1) + parseInt(cheese1);
+      toppingsText = ' - Beef and Cheese';
     } else if (cheese.checked == true) {
       var cheese2 = document.getElementById('cheese').value;
       pepperoniToppings = parseInt(cheese2);
+      toppingsText = ' - Cheese';
     } else if (beef.checked == true) {
       var beef2 = document.getElementById('beef').value;
       pepperoniToppings = parseInt(beef2);
+      toppingsText = ' - Beef';
     } else {
       return alert('please select atleast one topping');
     }
@@ -60,7 +69,25 @@ export function pepperoni() {
     var orderToppings = addToPepperoniOrder.pepperoniToppings;
     var orderTotal = addToPepperoniOrder.pepperoniTotal;
 
-    const newOrder = `<div class="card-one"><div><p class="title-one">Pepperoni</p><p class="price-one">KES ${orderName}</p></div><div><p class="title-one">Crust</p><p class="price-one">KES ${orderCrust}</p></div><div><p class="title-one">Toppings</p><p class="price-one">KES ${orderToppings}</p></div><div><p class="title-one">Total</p><p class="price-one">KES ${orderTotal}</p></div></div>`;
+    if (orderName == '499') {
+      optionText = ' - Small';
+    } else if (orderName == '749') {
+      optionText = ' - Medium';
+    } else if (orderName == '1099') {
+      optionText = ' - Large';
+    } else if (orderName == '1399') {
+      optionText = ' - Deluxe';
+    }
+
+    if (orderCrust == '99') {
+      crustText = ' - Single';
+    } else if (orderCrust == '149') {
+      crustText = ' - Double';
+    } else if (orderCrust == '199') {
+      crustText = ' - Mega';
+    }
+
+    const newOrder = `<div class="card-one"><div><p class="title-one">Pepperoni${optionText}</p><p class="price-one">KES ${orderName}</p></div><div><p class="title-one">Crust${crustText}</p><p class="price-one">KES ${orderCrust}</p></div><div><p class="title-one">Toppings${toppingsText}</p><p class="price-one">KES ${orderToppings}</p></div><div><p class="title-one">Total</p><p class="price-one">KES ${orderTotal}</p></div></div>`;
 
     const list = document.querySelector('#orderCard');
     list.insertAdjacentHTML('afterbegin', newOrder);
@@ -68,29 +95,32 @@ export function pepperoni() {
     subtotal = subtotal + orderTotal;
     grandTotal = subtotal + 250;
 
-    const orderCheckout = document.querySelector('#orderCheckoutGrandTotal');
-    const newGrandTotal = `<span class="t_price">${grandTotal}</span>`;
-    orderCheckout.insertAdjacentHTML('afterbegin', newGrandTotal);
-
-    var subtotalText = `<p class="name">Subtotal</p><p class="total-price">KES <span class="st_price">${subtotal}</span></p>`;
-
-    var subtotalTrue = document.querySelector('#subtotalTrue');
-
-    subtotalTrue.insertAdjacentHTML('afterbegin', subtotalText);
+    var subtotalTrue = document.getElementById('subtotalTrue');
+    subtotalTrue.innerHTML = 'KES ' + subtotal;
 
     locationOrder.addEventListener('click', function (e) {
       e.preventDefault();
       locationModal.style.display = 'none';
 
-      var locationText = `<p class="name">${location.value}</p><p class="total-price">KES <span class="st_price">250</span></p>`;
-      var locationEmpty = `<p>Please add a location</p>`;
-      var locationVal = document.querySelector('#locationVal');
-      var locationTrue = document.querySelector('#locationTrue');
+      var locationText = document.getElementById('location').value;
+      var locationPrice = document.getElementById('locationPrice');
+      var locationEmpty = 'Please add a location';
+      var locationVal = document.getElementById('locationVal');
+      var locationTrue = document.getElementById('locationTrue');
 
       if (location.value == '') {
-        locationVal.insertAdjacentHTML('afterbegin', locationEmpty);
+        locationVal.innerHTML = locationEmpty;
+        const orderCheckout = document.getElementById(
+          'orderCheckoutGrandTotal'
+        );
+        orderCheckout.innerHTML = 'KES ' + grandTotal;
       } else {
-        locationTrue.insertAdjacentHTML('afterbegin', locationText);
+        locationTrue.innerHTML = locationText;
+        locationPrice.innerHTML = 'KES 250';
+        const orderCheckout = document.getElementById(
+          'orderCheckoutGrandTotal'
+        );
+        orderCheckout.innerHTML = 'KES ' + grandTotal;
       }
     });
 
@@ -98,8 +128,25 @@ export function pepperoni() {
       e.preventDefault();
 
       var checkoutVal = document.querySelector('#checkoutVal');
+      var locationText = document.getElementById('location').value;
+      var locationTrue = document.getElementById('locationTrue');
+      var locationPrice = document.getElementById('locationPrice');
 
-      checkoutVal.innerHTML = '<p>Thanks for choosing Pizzark</p>';
+      if (location.value == '') {
+        locationModal.style.display = 'block';
+      } else {
+        locationTrue.innerHTML = locationText;
+        locationPrice.innerHTML = 'KES 250';
+        const orderCheckout = document.getElementById(
+          'orderCheckoutGrandTotal'
+        );
+        orderCheckout.innerHTML = 'KES ' + grandTotal;
+        checkoutVal.innerHTML =
+          'Thanks for choosing Pizzark We shall deliver to your location in ' +
+          locationText +
+          ' soon';
+      }
+      return reset;
     });
   });
 }
